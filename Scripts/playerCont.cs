31747e755cad63ea1b4d554a92e3e21b11ec8946
Sam Cookson <sbcskyline18@gmail.com>
2018-01-25 19:07:43 -0700

    Update playerCont.cs

--- Scripts/playerCont.cs
+++ Scripts/playerCont.cs
@@ -1,29 +1,42 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerCont : MonoBehaviour {
 
 		public float speed;
+		private bool onGround = false;
+		public float jumpForce;
+
 		private Rigidbody2D rb;
-		public float jump;
-		public GameObject rayOrigin;
-		public float rayCheckDistance;
+		private Vector2 jumping;
 
 	void Start () {
 		rb = GetComponent <Rigidbody2D> ();
 	}
 
 	void FixedUpdate () {
-		float x = Input.GetAxis ("Horizontal");
+		Jump();
+	}
 
-		if (Input.GetAxis ("Jump") > 0) {
-			RaycastHit2D hit = Physics2D.Raycast (rayOrigin.transform.position, Vector2.down, rayCheckDistance);
-			if (hit.collider != null) {
-				rb.AddForce (Vector2.up * jump, ForceMode2D.Impulse);
-			}
-		}
-		rb.velocity = new Vector2 (x * speed, rb.velocity.y);
+	public void Jump() {
+		if (onGround && Input.GetKeyDown(KeyCode.Space)) {
+       		rb.AddForce(new Vector2(0, jumpForce));
+    	} 
 
+    	if(Input.GetKeyDown(KeyCode.A)) {
+    		rb.velocity = new Vector2(-speed, 0);
+    	} else if(Input.GetKeyDown(KeyCode.D)) {
+    		rb.velocity = new Vector2(speed, 0);
+    	}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Ground") {
+			onGround = true;
+		} else {
+			onGround = false;
 		}
 	}
+}
