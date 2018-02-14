0689e77bfb3502bc044c2d6fd7e72abcdbff6f95
Sam Cookson <sbcskyline18@gmail.com>
2018-02-13 18:42:17 -0700

    Add files via upload

---
+++ Scripts/Sound.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+ 
+ [System.Serializable]
+ public class Sound : MonoBehaviour 
+ {
+
+	public string name;
+
+	public AudioClip clip;
+		[Range(0f, 1f)]
+	public float volume;
+		 [Range(.1f, 3f)]
+	public float pitch;
+}
