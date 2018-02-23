c79548e396ce1192b1b7249a6ff8b637b2f69fff
Sam Cookson <sbcskyline18@gmail.com>
2018-02-22 19:30:21 -0700

    Update DialogueTrigger.cs

--- Scripts/DialogueTrigger.cs
+++ Scripts/DialogueTrigger.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,21 @@ public class DialogueTrigger : MonoBehaviour {
 
 	public Dialogue dialogue;
 
+	public GameObject dBox;
+
+	void Start() {
+		dBox.SetActive(false);
+	}
+
 	public void TriggerDialogue ()
 	{
 		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
 	}
 
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.name == "Player") {
+			dBox.SetActive(true);
+			TriggerDialogue();
+		}
+	}
 }
