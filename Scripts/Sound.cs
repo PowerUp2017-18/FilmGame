09f50e4801e433a20636cf4d232c4cb6425bb2ec
Sam Cookson <sbcskyline18@gmail.com>
2018-02-22 19:31:57 -0700

    Update Sound.cs

--- Scripts/Sound.cs
+++ Scripts/Sound.cs
@@ -1,17 +1,21 @@
-﻿
-using UnityEngine;
-using System.Collections;
 using UnityEngine.Audio;
+using UnityEngine;
  
  [System.Serializable]
- public class Sound : MonoBehaviour 
- {
+ public class Sound {
 
 	public string name;
 
 	public AudioClip clip;
-		[Range(0f, 1f)]
+
+	[Range(0f, 1f)]
 	public float volume;
-		 [Range(.1f, 3f)]
+
+	[Range(.1f, 3f)]
 	public float pitch;
+
+	public bool loop;
+
+	[HideInInspector]
+	public AudioSource source;
 }
