93927c54dc356c4e23dd08a6550aaa834850e522
TheMoisturizer <35356493+TheMoisturizer@users.noreply.github.com>
2018-02-22 20:00:21 -0700

    Add files via upload

--- Scripts/ClampBoard.cs
+++ Scripts/ClampBoard.cs
@@ -1,10 +1,11 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClampBoard: MonoBehaviour {
 
     private Rigidbody2D rigid;
+    private Animator anim;
 
     public GameObject player;
     public float moveSpeed;
@@ -18,6 +19,8 @@ public class ClampBoard: MonoBehaviour {
     private bool onGround;
 
     void Start () {
+        anim = GetComponent<Animator>();
+
         rigid = this.GetComponent<Rigidbody2D>();
         StartCoroutine (JumpTimer());
         player = GameObject.FindGameObjectWithTag ("Player");
@@ -25,10 +28,15 @@ public class ClampBoard: MonoBehaviour {
 
     void Update () {
         Move ();
+    
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
     }
 
     public void Move() {
-        rigid.velocity = new Vector2(-moveSpeed, 0);
+        rigid.velocity = new Vector2(moveSpeed, 0);
     }
 
     IEnumerator JumpTimer() {
@@ -44,10 +52,17 @@ public class ClampBoard: MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.tag == "Wall") {
+        if (col.collider.GetType() == typeof(CapsuleCollider2D) && col.gameObject.tag == "Wall") {
+            moveSpeed *= -1;
+            Debug.Log("collided");
+        }
+        if (col.collider.GetType() == typeof(CapsuleCollider2D) && col.gameObject.tag == "Enemy") {
             moveSpeed *= -1;
         }
-        if(col.gameObject.tag == "Player") {
+        if (col.collider.GetType() == typeof(CapsuleCollider2D) && col.gameObject.tag == "Player") {
+            Destroy(col.gameObject);
+        }
+        if (col.collider.GetType() == typeof(BoxCollider2D) && col.gameObject.tag == "Player") {
             Destroy(col.gameObject);
         }
     }
