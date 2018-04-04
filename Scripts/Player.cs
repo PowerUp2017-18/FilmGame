877d4001eb8e599b3f5afb7524b52b8c9051db60
TheMoisturizer <35356493+TheMoisturizer@users.noreply.github.com>
2018-04-03 19:58:33 -0600

    Update Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,14 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using UnityEngine.SceneManagement;
+
 
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
 	public float jumpHeight = 4;
 	public float timeToJumpApex = .4f;
-	public Text countText;
-	private int count;
 	float accelerationTimeAirborne = .2f;
 	float accelerationTimeGrounded = .1f;
 	float moveSpeed = 6;
@@ -20,18 +21,16 @@ public class Player : MonoBehaviour {
 
 	Controller2D controller;
 
-	void Start() {
+	void Start () {
+		
 		controller = GetComponent<Controller2D> ();
 
 		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
 		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
-	
-		count = 0;
-        SetCountText ();
-		}
+	}
 
-	void Update() {
+	void Update () {
 
 		if (controller.collisions.above || controller.collisions.below) {
 			velocity.y = 0;
@@ -48,22 +47,4 @@ public class Player : MonoBehaviour {
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move (velocity * Time.deltaTime);
 	}
-
-	void OnTriggerEnter(Collider other) {
-        
-        if (other.gameObject.CompareTag ("Prop")) {
-            other.gameObject.SetActive (false);
-            count = count + 1;
-            SetCountText ();
-        }
-    }
-
-    void SetCountText () {
-        
-        GameObject[] Prop = GameObject.FindGameObjectsWithTag("Prop");
-        int total = Prop.Length;
-        countText.text = count.ToString () + "/" + total ;
-    }
-
-
 }
