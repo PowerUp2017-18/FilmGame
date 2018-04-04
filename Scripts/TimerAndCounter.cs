486f8293ffa1068bbbcab4890c24e435ecf3b092
TheMoisturizer <35356493+TheMoisturizer@users.noreply.github.com>
2018-04-03 19:57:42 -0600

    Add files via upload

---
+++ Scripts/TimerAndCounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TimerAndCounter : MonoBehaviour {
+
+	public Text countText;
+	public Text timerText;
+	private int count;
+
+	float timer = 60.0f;
+
+	void Start () {
+		
+		count = 0;
+		SetCountText ();
+		timer = 60.0f;
+	}
+	
+	void Update () {
+		
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+     	Application.LoadLevel ("EndScreen");
+     	timerText.text = " " + Mathf.Round (timer);
+     	}
+	}
+	
+	void OnTriggerEnter (Collider other) {
+        
+        if (other.gameObject.CompareTag ("Prop")) {
+            count = count + 1;
+            SetCountText ();
+        }
+    	
+    	Debug.Log (count);
+    }
+    
+    void SetCountText () {
+        
+        countText.text = " " + count.ToString ();
+
+        if (count >= 12) {
+			Application.LoadLevel ("EndScreen");
+		}
+    }
+}
